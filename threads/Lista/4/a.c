63e434b4d77047487180bd8a331cf50ccc31b95e
gbrls <gabrielschneider100@protonmail.com>
2020-10-25 13:36:01 -0300

    Esqueleto

---
+++ threads/Lista/4/a.c
@@ -0,0 +1,12 @@
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// número máximo de threads em execução.
+#define N 10
+
+int main() {
+    return 0;
+}
